00c08b25bef912431c6d611779430f072a098d58
eddyerburgh <edward.yerburgh@gmail.com>
2018-07-16 23:06:15 +0100

    Set babel env vars in babe.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
+process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true
+process.env.VUE_CLI_BABEL_TARGET_NODE = 'node'
+
 module.exports = {
   presets: [
     '@vue/app'
   ]
 }
+
+// "test:unit:debug": "cross-env VUE_CLI_BABEL_TRANSPILE_MODULES=true VUE_CLI_BABEL_TARGET_NODE=true node --inspect-brk ./node_modules/jest/bin/jest.js --no-cache"
