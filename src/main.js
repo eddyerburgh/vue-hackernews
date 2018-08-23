083bc851e03d7d5c61f19dab0bdf6bfb423d6eec
eddyerburgh <edward.yerburgh@gmail.com>
2018-08-23 19:38:52 +0100

    Refactor fetchListData

--- src/main.js
+++ src/main.js
@@ -4,15 +4,11 @@ import { fetchListData } from './api/api'
 
 Vue.config.productionTip = false
 
-function getTopItems () {
-  return fetchListData('top')
-    .then(items => items)
-}
-
-getTopItems().then((items) => {
-  window.items = items
-  new Vue({
-    el: '#app',
-    render: h => h(App)
+fetchListData('top')
+  .then((items) => {
+    window.items = items
+    new Vue({
+      el: '#app',
+      render: h => h(App)
+    })
   })
-})
